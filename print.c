da62d9d473190005b3cd6db3ded29b745763e56e
KarolinaBysiewicz <116909609+KarolinaBysiewicz@users.noreply.github.com>
2024-01-22 11:42:24 +0100

    print.c

---
+++ print.c
@@ -0,0 +1,21 @@
+#include "print.h"
+
+void PrintAll(int a, Student students[a]);
+{
+}
+
+void PrintByName(int a, Student students[a]);
+{
+}
+
+void PrintBySurname(int a, Student students[a]);
+{
+}
+
+void PrintByIndexNr(int a, Student students[a]);
+{
+}
+
+void PrintByAvg(int a, Student students[a]);
+{
+}
